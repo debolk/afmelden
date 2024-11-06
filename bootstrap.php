0f1fdb452104037a67471c991872d9d47df8ea67
Jakob Buis <jakob@jakobbuis.nl>
2024-11-06 12:32:38 +0100

    better output of environment errors

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 
+use Dotenv\Exception\ExceptionInterface;
+
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$dotenv->required('HCAPTCHA_SITEKEY')->notEmpty();
-$dotenv->required('HCAPTCHA_SECRET')->notEmpty();
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    $dotenv->required('HCAPTCHA_SITEKEY')->notEmpty();
+    $dotenv->required('HCAPTCHA_SECRET')->notEmpty();
+}
+catch (ExceptionInterface $e) {
+    http_response_code(500);
+    echo "Missing environment variables; double-check installation instructions";
+    exit;
+}
