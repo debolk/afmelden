2dc9c53bf3230f3cf453dea950450ad2d4bd4136
Jakob Buis <jakob@jakobbuis.nl>
2024-11-06 12:27:36 +0100

    require specific environment keys

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,5 @@ require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+$dotenv->required('HCAPTCHA_SITEKEY')->notEmpty();
+$dotenv->required('HCAPTCHA_SECRET')->notEmpty();
